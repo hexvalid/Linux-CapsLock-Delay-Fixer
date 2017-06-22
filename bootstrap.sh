40ad913f872256ab6d93ad1eb15c015443aaf967
David Grenier <davidgrenier@gmail.com>
2017-06-22 09:05:47 -0400

    Fixes issue with source path and clean up

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 #Linux CapsLock Delay Fixer by ErkanMDR /- HELYX
+
+cd "${BASH_SOURCE%/*}" || exit
 
 xkbcomp -xkb $DISPLAY xkbmap
 fixpatchline=$(cat fixpatch)
 perl -i~ -0777 -pe "s/key .CAPS[^}]+};/$fixpatchline/" xkbmap
 xkbcomp xkbmap $DISPLAY
-clear
+rm xkbmap*
 echo "Problem fixed ;)"
