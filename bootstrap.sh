26523b895cc4e320ddbbf4dbdd617689c820fe4d
David Grenier <davidgrenier@gmail.com>
2017-06-22 08:34:40 -0400

    Fixes relative path

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 #Linux CapsLock Delay Fixer by ErkanMDR /- HELYX
 
 xkbcomp -xkb $DISPLAY xkbmap
-fixpatchline=$(cat /home/david/projects/capslock/fixpatch)
+fixpatchline=$(cat fixpatch)
 perl -i~ -0777 -pe "s/key .CAPS[^}]+};/$fixpatchline/" xkbmap
 xkbcomp xkbmap $DISPLAY
 clear
