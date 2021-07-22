98e0ba763e3a63b42551f4d2a3e3d0f99d2135eb
kp <77463574+pedrokpp@users.noreply.github.com>
2021-07-22 18:00:28 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
-#Linux CapsLock Delay Fixer by ErkanMDR /- HELYX
+#!/bin/sh
+rpl='key <CAPS> \{ repeat=no, type\[group1\]=\"ALPHABETIC\", symbols\[group1\]=\[ Caps_Lock, Caps_Lock \],actions\[group1\]=\[LockMods\(modifiers=Lock\),Private\(type=3,data\[0\]=1,data\[1\]=3,data\[2\]=3\) \] \}'
 
-cd "${BASH_SOURCE%/*}" || exit
+# Create copy of kb description
+xkbcomp -xkb $DISPLAY keyboardmap
 
-xkbcomp -xkb $DISPLAY xkbmap
-fixpatchline=$(cat fixpatch)
-perl -i~ -0777 -pe "s/key .CAPS[^}]+};/$fixpatchline/" xkbmap
-xkbcomp xkbmap $DISPLAY
-rm xkbmap*
-echo "Problem fixed ;)"
+# Replace CAPS
+sed -i "s/key <CAPS>[^;]*/$rpl/" keyboardmap
+
+# Apply
+xkbcomp keyboardmap $DISPLAY
+
+# Remove temp file
+rm keyboardmap
+
+# script provided by ben2talk and tprei at https://github.com/hexvalid/Linux-CapsLock-Delay-Fixer/issues/12
